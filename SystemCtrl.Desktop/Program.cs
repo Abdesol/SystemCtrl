29417f8e3cbc47a67a0a2c43ff0a1bf414f12099
abdesol <abdesoltak@gmail.com>
2026-07-19 15:19:47 +0300

    draft desktop/avalonia project

---
+++ SystemCtrl.Desktop/Program.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using ReactiveUI.Avalonia;
+using System;
+
+namespace SystemCtrl.Desktop;
+
+sealed class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+#if DEBUG
+            .WithDeveloperTools()
+#endif
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI(_ => { });
+}
