f0abb3df36f1ef7b844f05b2c4b754eb3698477e
abdesol <abdesoltak@gmail.com>
2026-07-22 19:00:30 +0300

    update MainWindow title and window decorations; implement drag-to-move functionality

--- src/SystemCtrl.Desktop/Views/MainWindow.axaml.cs
+++ src/SystemCtrl.Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace SystemCtrl.Desktop.Views;
 
@@ -7,5 +8,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void ChangeWindowPosition(object sender, PointerPressedEventArgs e)
+    {
+        base.OnPointerPressed(e);
+        BeginMoveDrag(e);
     }
 }
