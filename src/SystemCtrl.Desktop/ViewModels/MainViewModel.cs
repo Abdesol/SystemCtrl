870999d96d48aacdc99e5a8b08f9688bc12f2d94
abdesol <abdesoltak@gmail.com>
2026-07-24 19:36:42 +0300

    feat: add search functionality to MainViewModel and UI; implement service filtering and refresh capabilities

--- src/SystemCtrl.Desktop/ViewModels/MainViewModel.cs
+++ src/SystemCtrl.Desktop/ViewModels/MainViewModel.cs
@@ -6,27 +6,53 @@ using ReactiveUI.SourceGenerators;
 using SystemCtrl.Core.Interfaces;
 using SystemCtrl.Core.Models;
 
+using System.Linq;
+using System.Collections.Generic;
+
 namespace SystemCtrl.Desktop.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     private readonly IServiceProvider _serviceProvider;
     private readonly IWindowsServiceManager _windowsServiceManager;
+    private List<WindowsServiceInfo> _allServices;
 
     public MainViewModel(IServiceProvider serviceProvider, IWindowsServiceManager windowsServiceManager)
     {
         _serviceProvider = serviceProvider;
         _windowsServiceManager = windowsServiceManager;
 
-        Services = new ObservableCollection<WindowsServiceInfo>(_windowsServiceManager.GetServices());
+        _allServices = _windowsServiceManager.GetServices().ToList();
+        Services = new ObservableCollection<WindowsServiceInfo>(_allServices);
         SlidePanel = new SlidePanelViewModel();
+
+        this.WhenAnyValue(x => x.SearchText)
+            .Subscribe(_ => FilterServices());
     }
 
     [Reactive]
     public partial ObservableCollection<WindowsServiceInfo> Services { get; set; }
 
     [Reactive]
+    public partial string SearchText { get; set; } = string.Empty;
+
+    [Reactive]
     public partial SlidePanelViewModel SlidePanel { get; set; }
+
+    private void FilterServices()
+    {
+        if (string.IsNullOrWhiteSpace(SearchText))
+        {
+            Services = new ObservableCollection<WindowsServiceInfo>(_allServices);
+        }
+        else
+        {
+            var query = SearchText.ToLowerInvariant();
+            Services = new ObservableCollection<WindowsServiceInfo>(_allServices.Where(s => 
+                s.DisplayName.ToLowerInvariant().Contains(query) || 
+                s.ServiceName.ToLowerInvariant().Contains(query)));
+        }
+    }
 
     [ReactiveCommand]
     public void OpenService(WindowsServiceInfo selectedService)
@@ -42,5 +68,12 @@ public partial class MainViewModel : ViewModelBase
         var settingsViewModel = _serviceProvider.GetRequiredService<SettingsViewModel>();
         settingsViewModel.OnSaved = () => SlidePanel.Close();
         SlidePanel.Open("Settings", settingsViewModel);
+    }
+
+    [ReactiveCommand]
+    public void RefreshServices()
+    {
+        _allServices = _windowsServiceManager.GetServices().ToList();
+        FilterServices();
     }
 }
