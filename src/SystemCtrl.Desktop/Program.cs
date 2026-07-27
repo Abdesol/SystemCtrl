31b95e6ed5384e28835c3893c2ee5e80a696299d
abdesol <abdesoltak@gmail.com>
2026-07-27 14:27:13 +0300

    feat: implement single-instance application constraint using a global mutex in Program.cs

--- src/SystemCtrl.Desktop/Program.cs
+++ src/SystemCtrl.Desktop/Program.cs
@@ -1,14 +1,28 @@
-﻿using Avalonia;
+using Avalonia;
 using ReactiveUI.Avalonia;
 using System;
+using System.Threading;
 
 namespace SystemCtrl.Desktop;
 
 sealed class Program
 {
+    private static Mutex? _mutex;
+
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        const string appName = "SystemCtrl.Desktop.SingleInstanceMutex";
+        _mutex = new Mutex(true, appName, out bool createdNew);
+
+        if (!createdNew)
+        {
+            return;
+        }
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
